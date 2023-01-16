1015e656f5717169b194174a68aec172fd1ed635
Hugo MATHIEU <44479509+guedesite@users.noreply.github.com>
2023-01-16 23:00:35 +0100

    Fix event for switch theme

--- app/src/main/java/fr/luminy_lifi/app/MainActivity4.java
+++ app/src/main/java/fr/luminy_lifi/app/MainActivity4.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
@@ -31,10 +32,11 @@ public class MainActivity4 extends AppCompatActivity {
         //Permet de sauvegarder les preferences en cas d'arret par l'utilisateur de l'appli
 
 
-        switcher.setOnClickListener(new View.OnClickListener() {
+
+        switcher.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                MainActivity.instance.setSombre(((Switch)v).isActivated());
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                MainActivity.instance.setSombre(b);
             }
         });
 
